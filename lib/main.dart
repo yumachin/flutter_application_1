938cdf8789048cf24d3be08fa4636f6035525416
yumachin <curryuma925@gmail.com>
2024-06-21 12:26:42 +0900

    counter works and binary counter

--- lib/main.dart
+++ lib/main.dart
@@ -4,88 +4,85 @@ void main() {
   runApp(const MyApp2());
 }
 
+
 class MyApp2 extends StatelessWidget {
   const MyApp2({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "実践演習アプリ",
-      home: Scaffold(
-        appBar: AppBar(title: Text("実践演習アプリ")),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () => {}, child: Icon(Icons.airport_shuttle_outlined)),
-        body: ConstrainedBox(
-          constraints: BoxConstraints.expand(),
-          child: FlutterLogo(),
-      )),
-    );
+      home: MyHome());
   }
-  
 } 
 
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
+class MyHome extends StatefulWidget{
+  const MyHome({super.key});
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+  _MyHomeState createState() {
+    return _MyHomeState();
+  }  
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      
-      _counter++;
-    });
-  }
-
+class _MyHomeState extends State<MyHome> {
+  int count = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        
-        title: Text(widget.title),
-      ),
-      body: Center(
-        
-        child: Column(
-          
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+        appBar: AppBar(title: Text("実践演習アプリ")),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () => {
+            setState(() {  
+              count = 0;
+            })
+          }, child: Icon(Icons.airport_shuttle_outlined)),
+
+        body: Column(
+          children: [
+            Text("$count", style: TextStyle(fontSize: 80)),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                BinDigit(),
+                BinDigit(),
+                BinDigit(),
+                BinDigit(),
+                BinDigit(),
+                BinDigit(),
+                BinDigit(),
+                BinDigit()
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), 
-    );
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                  setState(() {
+                    count ++;
+                  });
+                  }, 
+                  child: Icon(Icons.plus_one)),
+
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() => count --);
+                  }, 
+                  child: Icon(Icons.exposure_minus_1)),
+            ],
+          )
+        ],)
+      );
+  }
+}
+
+
+class BinDigit extends StatelessWidget {
+  const BinDigit({super.key});
+  
+  @override
+  Widget build(BuildContext context) {
+    return Text("0");
   }
 }
